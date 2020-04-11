b97c8c4fa15ac1c6a9a206e688452e206c850963
Nizamuddin <nizamphoenix@gmail.com>
2020-04-11 16:55:14 +1000

    Update pipeline_driver.py

--- pipeline_driver.py
+++ pipeline_driver.py
@@ -9,5 +9,11 @@ from apache_beam.options.pipeline_options import PipelineOptions
 PipelineOptions has configurations for different aspects of your pipeline: runner-type, runner-specific configurations. 
 runner: runitme engine that executes pipelines like DataFlow(GCP), samza, flink etc
 '''
+somedata = ['eeeeeeeeeee','brrrr','aerrrrrr','garrrrrr','reaaera','afasfdasf']
 with beam.Pipeline(options=PipelineOptions()) as p:
-  pass
+  #1.reading data using beam's api
+  lines = p | 'ReadMyFile' >> beam.io.ReadFromText('gs://path_to_file_in_gcs')
+  #type(lines)-->PCollection
+  #2.reading data from system's memory
+  lines = (p | beam.Create(somedata))#Create is a transform
+  #A PCollection belongs to the Pipeline object for which it is created.Multiple pipelines cannot share a PCollection object.
