e2f4c8e2ebae839bfe6dd284005702ecf2eb696b
Nizamuddin <nizamphoenix@gmail.com>
2020-04-15 18:48:45 +1000

    Update pipeline_driver.py

--- pipeline_driver.py
+++ pipeline_driver.py
@@ -1,74 +1,108 @@
 '''
-This is a driver defining pipeline to run on GCP's DataFlow runner.
-This driver defines inputs, transforms, and outputs that constitute the pipeline.
+Batch processing pipeline:-
+1.reading input from local machine.
+2.Extracting data with ParDo transfrom.
+3.Applying composite transforms to compute useful information--scores.
+4.writing output to a text file.
+
+# Using DataflowRunner
+python scores.py \
+    --output gs://$BUCKET/scores/output \
+    --runner DataflowRunner \
+    --project $PROJECT_ID \
+    --region $REGION_ID \
+    --temp_location gs://$BUCKET/scores/temp
 '''
+from __future__ import absolute_import
+from __future__ import division
+
+import argparse
+import csv
+import logging
+
 import apache_beam as beam
+from apache_beam.metrics.metric import Metrics
 from apache_beam.options.pipeline_options import PipelineOptions
-
-'''
-PipelineOptions has configurations for different aspects of your pipeline: runner-type, runner-specific configurations. 
-runner: runitme engine that executes pipelines like DataFlow(GCP), samza, flink etc
-'''
-somedata = ['eeeeeeeeeee','brrrr','aerrrrrr','garrrrrr','reaaera','afasfdasf']
-options = PipelineOptions()
-google_cloud_options = options.view_as(GoogleCloudOptions)
-google_cloud_options.project = 'simple-data-pipelines'
-google_cloud_options.job_name = 'countjob'
-google_cloud_options.staging_location = 'gs://--------/staging'
-google_cloud_options.temp_location = 'gs://----------------/temp'
-options.view_as(StandardOptions).runner = 'DataflowRunner'
+from apache_beam.options.pipeline_options import SetupOptions
 
 
-with beam.Pipeline(options=PipelineOptions()) as p:
-  #1.reading data using beam's api
-  #lines = p | 'ReadMyFile' >> beam.io.ReadFromText('gs://path_to_file_in_gcs')
-  #type(lines)-->PCollection
-  #2.reading data from system's memory
-  #lines = (p | beam.Create(somedata))#Create is a transform 
-  # O/p PCollection = i/p PCollection | PCTransform
-  #A PCollection belongs to the Pipeline object for which it is created.Multiple pipelines cannot share a PCollection object.
-  p 
-  | beam.io.ReadFromText('gs://dataflow-samples/shakespeare/kinglear.txt')
-  | 'ExtractWords' >> beam.FlatMap(lambda x: re.findall(r'[A-Za-z\']+', x))
-  | beam.combiners.Count.PerElement()
-  | beam.MapTuple(lambda word, count: '%s: %s' % (word, count))
-  | beam.io.WriteToText('gs://my-bucket/counts.txt')
-  result = p.run()
-  
-  
-class ExtractAndFormat(beam.PTransform):
+class ParseGameEventFn(beam.DoFn):
+  """Parses the raw game event info into a Python dictionary.
+  Each event line has the following format:
+    username,teamname,score,timestamp_in_ms,readable_time
+  e.g.:
+    user2_AsparagusPig,AsparagusPig,10,1445230923951,2015-11-02 09:09:28.224
+  The human-readable time string is not used here.
   """
-  A composite transform to extract and format patient information.
+  def __init__(self):
+    beam.DoFn.__init__(self)
+    self.num_parse_errors = Metrics.counter(self.__class__, 'num_parse_errors')
+
+  def process(self, elem):
+    try:
+      row = list(csv.reader([elem]))[0]
+      yield {       #using yield is efficient,avoids memory explosion
+          'user': row[0],
+          'team': row[1],
+          'score': int(row[2]),
+          'timestamp': int(row[3]) / 1000.0,#converting to seconds
+      }
+    except:
+      # Log and count parse errors
+      self.num_parse_errors.inc()
+      logging.error('Parse error on "%s"', elem)
+
+
+
+class ExtractAndSumScore(beam.PTransform):
+  """
+  A transform to extract key/score information and sum the scores.
+  The constructor argument `field` determines whether 'team' or 'user' info is extracted.
   """
   def __init__(self, field):
-    super().__init__()
     beam.PTransform.__init__(self)
     self.field = field
 
   def expand(self, pcoll):
     return (
         pcoll
-        | beam.Map(lambda x: (x[self.field], x['score']))
-        | beam.CombinePerKey(sum))
-  
-  
-def run(argv=None, save_main_session=True):
+        | beam.Map(lambda elem: (elem[self.field], elem['score']))
+        | beam.CombinePerKey(sum)
+    )
 
+
+
+class UserScore(beam.PTransform):
+  def expand(self, pcoll):
+    return (
+        pcoll
+        | 'ParseGameEventFn' >> beam.ParDo(ParseGameEventFn())
+        # Extract and sum username/score pairs from the event data.
+        | 'ExtractAndSumScore' >> ExtractAndSumScore('user')
+    )
+
+
+
+def run(argv=None, save_main_session=True):
+  """Main entry point; defines and runs the user_score pipeline."""
   parser = argparse.ArgumentParser()
+
+  # The default maps to two large Google Cloud Storage files (each ~12GB)
+  # holding two subsequent day's worth (roughly) of data.
   parser.add_argument(
       '--input',
       type=str,
-      default='**************',
-      help='absolute path to data files.')
+      default='gs://apache-beam-samples/game/gaming_data*.csv',
+      help='Path to the data file(s) containing game data.')
   parser.add_argument(
-      '--output', 
-      type=str, 
-    required=True, 
-    help='Path to the output file(s).')
+      '--output', type=str, required=True, help='Path to the output file(s).')
 
   args, pipeline_args = parser.parse_known_args(argv)
 
   options = PipelineOptions(pipeline_args)
+
+  # We use the save_main_session option because one or more DoFn's in this
+  # workflow rely on global context (e.g., a module imported at module level).
   options.view_as(SetupOptions).save_main_session = save_main_session
 
   with beam.Pipeline(options=options) as p:
@@ -77,10 +111,14 @@ def run(argv=None, save_main_session=True):
       (user, score) = user_score
       return 'user: %s, total_score: %s' % (user, score)
 
-    (   
+    (  
         p
-        | 'ReadInputText' >> beam.io.ReadFromText(args.input)
-        | 'UserReport' >> UserReport()
-        | 'FormatPattientReports' >> beam.Map(format_patient_reports)
-        | 'WriteUserReports' >> beam.io.WriteToText(args.output)
+        | 'ReadInputText' >> beam.io.ReadFromText(args.input)#Step1
+        | 'UserScore' >> UserScore()#Step2
+        | 'FormatUserScoreSums' >> beam.Map(format_user_score_sums)#Step3
+        | 'WriteUserScoreSums' >> beam.io.WriteToText(args.output)#Step4
     )
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
